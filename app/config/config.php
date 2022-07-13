236b2623cbbc18afd0a5e8324fcdb6c1fb83d2cb
Denmar Ermitano <ermitano.den@gmail.com>
2022-07-13 13:54:53 -0400

    base app

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/MVC');
+  // Site Name
+  define('SITENAME', '420-411-VA: PHP MVC Application');
+
+  // Database Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+  define('DB_NAME', 'users');
